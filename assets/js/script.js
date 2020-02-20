e49b504e2c5964369e804ad367c6a8c8958dbd7f
adarsh1997shah <adarshshah01@gmail.com>
2020-02-20 22:05:33 +0530

    home page custumization

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
+// Function to show the sidebar.
+$(document).ready(function(){
+    $('.ca-nav-link-resposive').click( function() {
+        // alert('asdfaf');
+        $('.ca-sidebar').addClass('ca-sidebar-visible');
+    } );
+});
+
+
+// Function to show hide the sidebar if anywhere outside is clicked.
+$(document).click( function(event) {
+    if (event.target.className != 'fa fa-bars' ) {
+        console.log('yes');
+        $(".ca-sidebar").removeClass('ca-sidebar-visible');
+    }else{
+        console.log('no');
+    }
+});
+
+
+
+
 $(document).ready(function(){
     $('#ca-submit').on( 'click', function() {
         var id = $('#ca-id').val();
