769abb97234c2f7baaa688fd2fc0ba07261c9a84
jainankur770 <jain.ankur770@gmail.com>
2020-02-22 17:29:28 +0530

    Created signup for elder and young login of young is still pending

--- Api.py
+++ Api.py
@@ -17,23 +17,65 @@ def index():
     users = request.args.get("query")
     print(users)
     if users=="Login":
-        user=request.args.get("Id")
-        print(user)
-        d="SELECT Name,Age,Fund_raised,Contact,Review,Rating FROM elder_details WHERE Id"+"="+user+";"
-        id="SELECT Taken_care_by FROM elder_details WHERE Id" + "="+user+";"
-        cursor.execute(d)
-        rows = list(cursor.fetchone())
-        cursor.execute(id)
-        id=cursor.fetchone()
-        print(id)
-        for Id in id:
-            Taken_care_by="SELECT youth_details.Name FROM elder_details INNER JOIN youth_details ON elder_details.Taken_care_by=youth_details.Id Where elder_details.Taken_care_by"+"="+str(Id)+";"
-            print(Taken_care_by)
-            cursor.execute(Taken_care_by)
-            Taken_care_by=cursor.fetchone()
-        #rows=rows.append(Taken_care_by)
-        j=jsonify(rows,Taken_care_by)
-        return(j)
+        user_type=request.args.get("user_type")
+        if user_type =="Old":
+            user=request.args.get("Id")
+            print(user)
+            d="SELECT Name,Age,Fund_raised,Contact,Review,Rating FROM elder_details WHERE Id"+"="+user+";"
+            id="SELECT Taken_care_by FROM elder_details WHERE Id" + "="+user+";"
+            cursor.execute(d)
+            rows = list(cursor.fetchone())
+            cursor.execute(id)
+            id=cursor.fetchone()
+            print(id)
+            for Id in id:
+                Taken_care_by="SELECT youth_details.Name FROM elder_details INNER JOIN youth_details ON elder_details.Taken_care_by=youth_details.Id Where elder_details.Taken_care_by"+"="+str(Id)+";"
+                print(Taken_care_by)
+                cursor.execute(Taken_care_by)
+                Taken_care_by=cursor.fetchone()
+            #rows=rows.append(Taken_care_by)
+            j=jsonify(rows,Taken_care_by)
+            return(j)
+        elif user_type=="Young":
+            #young login
+            print("to be made")
+    elif users=="Signup":
+        print(users)
+        user_type=request.args.get("user_type")
+        if user_type =="Old":
+            name=request.args.get("name")
+            age=request.args.get("age")
+            fund_raised=request.args.get("fund_raised")
+            password= request.args.get("password")
+            contact_no=request.args.get("contact_no")
+            sql="INSERT INTO elder_details(Password,Name,Age,Fund_raised,Contact) VALUES (%s,%s, %s,%s,%s)"
+            val=(str(password),str(name),age,fund_raised,contact_no)
+            print(sql,val)
+            cursor.execute(sql,val)
+            connection.commit()
+            id="SELECT Id FROM elder_details Where Contact"+"="+str(contact_no)+";"
+            print(id)
+            cursor.execute(id) 
+            i=list(cursor.fetchone())
+            i=jsonify(i[0])
+            print(i)
+            return(i)
+        elif user_type=="Young":
+            name=request.args.get("name")
+            age=request.args.get("age")
+            address=request.args.get("address")
+            password= request.args.get("password")
+            contact_no=request.args.get("contact_no")
+            sql="INSERT INTO youth_details(Password,Name,Age,Address,Contact) VALUES (%s,%s, %s,%s,%s)"
+            val=(str(password),str(name),age,str(address),contact_no)
+            cursor.execute(sql,val)
+            connection.commit()
+            id="SELECT Id FROM youth_details Where Contact"+"="+str(contact_no)+";"
+            cursor.execute(id) 
+            i=list(cursor.fetchone())
+            i=jsonify(i[0])
+            return(i)
+        
 
 if __name__ == "__main__":
     ank.run(debug=True,host='127.0.0.1', port=5000)
